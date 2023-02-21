bd7898af592f816e4138dd981e032a6145750aa1
FestusCode <festusdjossou@gmail.com>
2023-02-21 00:43:15 +0000

    updated and styles review

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import ImageViewer from "./Components/ ImageViewer";
-import Button from "./Components/Buttons";
+import { StyleSheet, View } from "react-native";
+import ImageViewer from "./Components/ImageViewer";
+
+import Button from "./Components/Button";
 const placeholderImage = require("./assets/Images/background-image.png");
 export default function App() {
   return (
@@ -28,9 +29,11 @@ const styles = StyleSheet.create({
   imgContainer: {
     flex: 1,
     paddingTop: 58,
+    alignItems: "center",
   },
   btnsContainer: {
     flex: 1 / 3,
+    flexDirection: "column",
     alignItems: "center",
   },
 });
