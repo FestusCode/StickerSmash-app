d54bf0ffefc422ac8427db49a787ab871c5dec52
FestusCode <festusdjossou@gmail.com>
2023-02-21 19:58:39 +0000

    useState used to allow user to load an picture with launchImageLibraryAsync from ImagePicker

--- App.js
+++ App.js
@@ -1,17 +1,39 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import ImageViewer from "./Components/ImageViewer";
-
 import Button from "./Components/Button";
 const placeholderImage = require("./assets/Images/background-image.png");
+
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("you didn't select any picture.");
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imgContainer}>
-        <ImageViewer placeholderImageSource={placeholderImage} />
+        <ImageViewer
+          placeholderImageSource={placeholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.btnsContainer}>
-        <Button label="Choose a picture" />
+        <Button
+          theme="primary"
+          label="Choose a picture"
+          onPress={pickImageAsync}
+        />
         <Button label="Use picture above" />
       </View>
       <StatusBar style="auto" />
@@ -34,6 +56,5 @@ const styles = StyleSheet.create({
   btnsContainer: {
     flex: 1 / 3,
     flexDirection: "column",
-    alignItems: "center",
   },
 });
