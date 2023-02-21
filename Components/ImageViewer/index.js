2d237c37f5dc2838304910a7ac3c0e44e0f0eb82
FestusCode <festusdjossou@gmail.com>
2023-02-21 19:49:57 +0000

    Image source updated with conditional rending

--- Components/ImageViewer/index.js
+++ Components/ImageViewer/index.js
@@ -1,7 +1,9 @@
 import { StyleSheet, Image } from "react-native";
 
-const ImageViewer = ({ placeholderImageSource }) => {
-  return <Image source={placeholderImageSource} style={styles.image} />;
+const ImageViewer = ({ placeholderImageSource, selectedImage }) => {
+  const imageSource =
+    selectedImage !== null ? { uri: selectedImage } : placeholderImageSource;
+  return <Image source={imageSource} style={styles.image} />;
 };
 const styles = StyleSheet.create({
   image: {
